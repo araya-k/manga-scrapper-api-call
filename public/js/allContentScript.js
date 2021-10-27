0d2cf6378fea505f9f7057f709fb9307f0df9ba5
nikesuputra-win10 <nike.suputra@gmail.com>
2021-10-28 05:49:37 +0800

    Added image compression process

--- public/js/allContentScript.js
+++ public/js/allContentScript.js
@@ -2,35 +2,36 @@ const mangaName = document.URL.split('/').reverse()[1]
 const mangaChapter = document.URL.split('/').pop()
 const nextChapter = document.getElementById('next-chapter')
 
-// Call the API to get manga list data
-const fetchContentImages = (callback) => {
-    fetch(`/allcontent/${mangaName}/${mangaChapter}`, {
-        method: 'GET',
-    })
-        // Return the data from the server
-        .then(response => {
-            return response.json()
-        })
-        // Calling a callback function with data from the server
-        .then(jsonData => callback(jsonData))
-        // Show an alert if there is any error
-        .catch(err => {
-            alert('API Could not be reached at this time')
-            console.error(err)
-        })
+async function fetchContentSourceUrl() {
+    const url = `/allcontent/${mangaName}/${mangaChapter}`
+    try {
+        const res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        alert('API Could not be reached at this time')
+        console.log(error);
+    }
 }
 
-// Make the fetched data displayed in front end
-const displayContent = jsonData => {
-    jsonData.forEach(item => {
-        const itemName = item.split('/').pop()
-        const template = `<img src="${item}" alt="${mangaChapter} - ${itemName}">`
-        document.getElementById('template-content').innerHTML += template
-    })
+async function displayContent() {
+    const contentSourceUrl = await fetchContentSourceUrl()
+    await contentSourceUrl.reduce(async (prev, i) => {
+        await prev
+        const url = `https://cdn.filestackcontent.com/AcjcqxtYTCGdeWpNrgnpaz/pjpg=quality:80/store/${i}`
+        try {
+            const res = await fetch(url)
+            const compressedContentUrl = await res.json()
+            const template = `<img src="${compressedContentUrl.url}" alt="${mangaChapter} - ${compressedContentUrl.filename}">`
+            document.getElementById('template-content').innerHTML += template
+        } catch (error) {
+            alert('Encountered issues during compression process')
+            console.log(error);
+        }
+    }, undefined)
     const chapterNumber = mangaChapter.split('-').pop()
     const nextChapterNumber = parseInt(chapterNumber) + 1
     const nextChapterSlug = mangaChapter.split('-').slice(0, -1).concat(nextChapterNumber).join('-')
     nextChapter.href = `/series/${mangaName}/${nextChapterSlug}`
 }
 
-fetchContentImages(displayContent)
+displayContent()
