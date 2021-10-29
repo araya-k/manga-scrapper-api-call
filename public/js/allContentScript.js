cf65e746146a6c90a2f70fd8989c867ae1e8219c
nikesuputra-win10 <nike.suputra@gmail.com>
2021-10-29 17:56:55 +0800

    Added backup process for image compression

--- public/js/allContentScript.js
+++ public/js/allContentScript.js
@@ -1,6 +1,7 @@
 const mangaName = document.URL.split('/').reverse()[2]
 const mangaChapter = document.URL.split('/').pop()
 const nextChapter = document.getElementById('next-chapter')
+const loader = document.getElementById('loader')
 
 async function fetchContentSourceUrl() {
     const url = `/allcontent/${mangaName}/${mangaChapter}`
@@ -14,21 +15,38 @@ async function fetchContentSourceUrl() {
 }
 
 async function displayContent() {
+    loader.style.display = 'block'
     const contentSourceData = await fetchContentSourceUrl()
     const contentSourceUrl = await contentSourceData[0].content
-    await contentSourceUrl.reduce(async (prev, i) => {
-        await prev
-        const url = `https://cdn.filestackcontent.com/AcjcqxtYTCGdeWpNrgnpaz/pjpg=quality:80/store/${i}`
-        try {
+    try {
+        await contentSourceUrl.reduce(async (prev, i) => {
+            await prev
+            const url = `https://cdn.filestackcontent.com/AcjcqxtYTCGdeWpNrgnpaz/pjpg=quality:80/store/${i}`
             const res = await fetch(url)
-            const compressedContentUrl = await res.json()
-            const template = `<img src="${compressedContentUrl.url}" alt="${mangaName}-chapter-${mangaChapter}-${compressedContentUrl.filename}">`
-            document.getElementById('template-content').innerHTML += template
-        } catch (error) {
-            alert('Encountered issues during compression process')
-            console.log(error);
-        }
-    }, undefined)
+            if (res.status !== 200) {
+                const urlBackup = `https://cdn.filestackcontent.com/ADQyp66f8SG6PlRWd2pIQz/pjpg=quality:80/store/${i}`
+                const resBackup = await fetch(urlBackup)
+                if  (resBackup.status !== 200) {
+                    const template = `<img src="${i}" alt="${mangaName}-chapter-${mangaChapter}-${i.split('/').pop()}">`
+                    loader.style.display = 'none'
+                    document.getElementById('template-content').innerHTML += template
+                } else {
+                    const compressedContentUrl = await resBackup.json()
+                    const template = `<img src="${compressedContentUrl.url}" alt="${mangaName}-chapter-${mangaChapter}-${compressedContentUrl.filename}">`
+                    loader.style.display = 'none'
+                    document.getElementById('template-content').innerHTML += template
+                }
+            } else {
+                const compressedContentUrl = await res.json()
+                const template = `<img src="${compressedContentUrl.url}" alt="${mangaName}-chapter-${mangaChapter}-${compressedContentUrl.filename}">`
+                loader.style.display = 'none'
+                document.getElementById('template-content').innerHTML += template
+            }
+        }, undefined)
+    } catch (error) {
+        alert('Encountered issues while trying to fetch images')
+        console.log(error);
+    }
     const nextChapterNumber = parseInt(mangaChapter) + 1
     nextChapter.href = `/series/${mangaName}/chapter/${nextChapterNumber}`
 }
